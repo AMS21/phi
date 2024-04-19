26562b424873d1678d2eea8ea3bc350f42c63376
AMS21 <AMS21.github@gmail.com>
2024-04-19 11:53:46 +0200

    [CMake] Fix `include_guard` being added with CMake `3.10`

--- scripts/cmake/internal/IncludeGuard.cmake
+++ scripts/cmake/internal/IncludeGuard.cmake
@@ -4,14 +4,14 @@ endif()
 set("__PHI_INCLUDE_GUARD_INCLUDE_GUARD__" TRUE)
 
 macro(phi_include_guard)
-  if(${CMAKE_VERSION} VERSION_GREATER "3.9")
-    include_guard(GLOBAL)
-  else()
+  if(${CMAKE_VERSION} VERSION_LESS "3.10")
     # Workaround for older CMake versions
     if(DEFINED "__PHI_INCLUDE_GUARD_${CMAKE_CURRENT_LIST_FILE}__")
       return()
     endif()
 
     set("__PHI_INCLUDE_GUARD_${CMAKE_CURRENT_LIST_FILE}__" TRUE)
+  else()
+    include_guard(GLOBAL)
   endif()
 endmacro()
