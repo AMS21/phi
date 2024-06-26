388a8fabdc202c1b1431cd482ec5e216005e2c94
AMS21 <AMS21.github@gmail.com>
2024-06-26 11:44:20 +0200

    [Misc] Add flake files

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Phi";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        llvm = pkgs.llvmPackages_18;
+      in {
+        devShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } rec {
+          packages = with pkgs; [
+            clang-tools_18
+            clang_18
+            cmake
+            cmake-format
+            cppcheck
+            gdb
+            include-what-you-use
+            llvm.libcxx
+            llvm.lldb
+            ninja
+            valgrind
+          ];
+        };
+      }
+    );
+}
